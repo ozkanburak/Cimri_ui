9bc6b52a873febadf59b3781f90536be2d2dae7b
Burak Özkan <ozkanburaak@gmail.com>
2022-08-31 19:40:21 +0300

    Initial Commit

---
+++ cimricom/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cimricom
+//
+//  Created by Burak Özkan on 31.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
